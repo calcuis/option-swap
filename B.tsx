0577e701dabfe22bb951c2113fa1eecc8e6b7d81
calcuis <113646141+calcuis@users.noreply.github.com>
2023-08-04 08:41:22 +0800

    Update B.tsx

--- B.tsx
+++ B.tsx
@@ -1,7 +1,7 @@
 const B = ({item,values}:any) => {
     return (
         <>
-            <li><input type="radio" value="b" name="choice" className="w-5 h-5" /> {item.b}{values.choice==item.ans && item.ans=="b" &&"✔️"}</li>
+            <li><input type="radio" value="b" name="choice" /> {item.b}{values.choice==item.ans && item.ans=="b" &&"✔️"}</li>
         </>
     );
 }
