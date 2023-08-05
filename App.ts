78fa56b008921c070ad2ca08fbdd0088ea6a9566
calcuis <113646141+calcuis@users.noreply.github.com>
2023-08-05 10:02:32 +0800

    Update App.ts

--- App.ts
+++ App.ts
@@ -38,11 +38,13 @@ function App() {
             {item.b&&!item.c&&<>
             {item.a&&<A item={item} values={values}/>}
             {item.b&&<B item={item} values={values}/>}
+            </>}
 
             {item.c&&(!item.d)&&(item.c!=="all of the above.")&&<>
             {item.a&&(swap==1?<B item={item} values={values}/>:(swap==2?<C item={item} values={values}/>:<A item={item} values={values}/>))}
             {item.b&&(swap==1?<A item={item} values={values}/>:(swap==3?<C item={item} values={values}/>:<B item={item} values={values}/>))}
             {item.c&&(swap==2?<A item={item} values={values}/>:(swap==3?<B item={item} values={values}/>:<C item={item} values={values}/>))}
+            </>}
 
             {item.d&&(item.d!=="all of the above.")&&<>
             {item.a&&(swap==4?<B item={item} values={values}/>:(swap==5?<C item={item} values={values}/>:(swap==6?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
@@ -50,6 +52,7 @@ function App() {
             {item.c&&(swap==4?<D item={item} values={values}/>:(swap==5?<A item={item} values={values}/>:(swap==6?<B item={item} values={values}/>:<C item={item} values={values}/>)))}
             {item.d&&(swap==4?<C item={item} values={values}/>:(swap==5?<B item={item} values={values}/>:(swap==6?<A item={item} values={values}/>:<D item={item} values={values}/>)))}
             {item.e&&<E item={item} values={values}/>}
+            </>}
 
             {(item.c=="all of the above."||item.d=="all of the above.")&&<>
             {item.a&&((swap==1 || (item.e && swap==4))?<B item={item} values={values}/>:(((item.d && swap==2) || (item.e && swap==5))?<C item={item} values={values}/>:((item.e && swap==6)?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
